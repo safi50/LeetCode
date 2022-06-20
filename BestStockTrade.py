81af44ea2d28136880ed137903011ab115ec30df
safi50 <63853856+safi50@users.noreply.github.com>
2022-06-20 19:11:26 +0500

    Create BestStockTrade.py

---
+++ BestStockTrade.py
@@ -0,0 +1,34 @@
+'''
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0
+
+Example 1:
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+'''
+
+#KADANE ALGORITHM 
+def maxProfit(self, prices: List[int]) -> int:
+
+	localmax , globalmax = 0 , 0
+	for i in range(1, len(prices), 1):   # start from the 2nd element or index 1
+		localmax = max(0, localmax + (prices[i] - prices[i-1])) # implement Kadane Algorithm 
+		if localmax > globalmax: 
+			globalmax = localmax
+		# globalmax = max(globalmax, localmax)
+	return globalmax
+
+
+
+# Another Correct Solution but Slow
+
+def maxProfit(self, prices: List[int]) -> int:
+	a = 0
+	for i in range(len(prices)-1):
+		a = max(a, (max(prices[i+1:]) - prices[i]))
+	return a
+
+        
