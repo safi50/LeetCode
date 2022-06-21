8ff47a95029b8779abdb2215b3fc2439647b0030
safi50 <63853856+safi50@users.noreply.github.com>
2022-06-21 20:57:49 +0500

    Update PascalTriangle.py

--- PascalTriangle.py
+++ PascalTriangle.py
@@ -5,9 +5,9 @@ In Pascal's triangle, each number is the sum of the two numbers:
 Example:
 NumRows = 6
 							1
-						1		1						1+1 = 2
-					1	    2		1					1+2 = 3 
-				1	    3       3		1				1+3 = 4, 3+3 = 6 ....
+						1		1									1+1 = 2
+					1		2		1								1+2 = 3 
+			1	    	3       3		1									1+3 = 4, 3+3 = 6 ....
 			1		4		6		4		1
 		1		5		10		10		5		1
 
