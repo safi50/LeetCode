605f974200e8a234081237f7cd51b80707749ea1
safi50 <63853856+safi50@users.noreply.github.com>
2022-06-21 21:03:19 +0500

    Update ReshapeMatrix.py

--- ReshapeMatrix.py
+++ ReshapeMatrix.py
@@ -1,4 +1,20 @@
+'''
+Question: 
+In MATLAB, there is a handy function called reshape which can reshape an m x n matrix into a new one with a different size r x c keeping its original data
+You are given an m x n matrix mat and two integers r and c representing the number of rows and the number of columns of the wanted reshaped matrix.
+The reshaped matrix should be filled with all the elements of the original matrix in the same row-traversing order as they were.
+If the reshape operation with given parameters is possible and legal, output the new reshaped matrix; Otherwise, output the original matrix
 
+Example:
+Input: mat = [[1,2],[3,4]], r = 1, c = 4
+Output: [[1,2,3,4]]
+'''
+
+'''
+Approach: 
+2D array is basically an array of arrays. So, we traverse the input array and fill individual inner rows first, and then append those rows 
+to the resultant array. 
+Res = [ [row1], [row2] , .... ] 
 '''
 # array = sum(array , []) --> converts a 2D array to a 1D array
 
