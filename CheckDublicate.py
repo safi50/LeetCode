245d62d2a084bad155e5dc99a38d29aee3553cdd
safi50 <63853856+safi50@users.noreply.github.com>
2022-06-21 21:02:22 +0500

    Update CheckDublicate.py

--- CheckDublicate.py
+++ CheckDublicate.py
@@ -1,4 +1,7 @@
-
+"""
+QUESTION STATEMENT:
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+"""
 
     def containsDuplicate(self, nums: List[int]) -> bool:
         
