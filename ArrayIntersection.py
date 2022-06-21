18a194c43a40737c8ee637c10e9d77bf1b06d99f
safi50 <63853856+safi50@users.noreply.github.com>
2022-06-21 21:01:35 +0500

    Update ArrayIntersection.py

--- ArrayIntersection.py
+++ ArrayIntersection.py
@@ -1,13 +1,4 @@
-'''
-Question: 
-Given two integer arrays nums1 and nums2, return an array of their intersection.
-Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
 
-Example:
-Input: nums1 = [1,2,2,1], nums2 = [2,2]
-Output: [2,2]
-
-'''
 
 def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]: # 2 list input parameters and 1 output list parameter
 	nums3 = []
