7ebd283472975057b79d4d354731b2da5086cc08
safi50 <63853856+safi50@users.noreply.github.com>
2022-06-22 15:37:57 +0500

    Update 2dArraySearch.py

--- 2dArraySearch.py
+++ 2dArraySearch.py
@@ -13,6 +13,7 @@ Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
 Output: false
  
 '''
+# For sorted arrays , Binary Search is the best algorithm and should be implemented. However, this solution is not that of binary search.
 
 def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
     for row in matrix:
