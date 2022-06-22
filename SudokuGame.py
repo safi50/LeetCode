01b665d7ad272565666baab8a746fab184407318
safi50 <63853856+safi50@users.noreply.github.com>
2022-06-22 14:58:34 +0500

    Update SudokuGame.py

--- SudokuGame.py
+++ SudokuGame.py
@@ -24,38 +24,6 @@ Output: false
 Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8.
 Since there are two 8's in the top left 3x3 sub-box, it is invalid.
 '''
-
-
-''' SOLUTION APPROACH
-Check for problems in Individual Rows, Columns, and Boxes.
-If any row , column, or box contains a dublicate number then we return false , else true.
-
-For Dublicates in Rows:
-Iterate over each element of individual arrays (rows) in the board, and check for dublicates.
-
-For Dublicates in Columns:
-Iterate over corresponding elements of each individual array.
-For Example:
-Suppose board = [ array1, array2, .... array9]
-so, array1[0] , array2[0] ...... array9[0] form the first column of the board.
-Similarly, using a for loop for the number of elements in an array (row), we can extract individual columns
-and check for dublicates using dictionaries as above.
-
-For Dublicates in Boxes:
-
-Checking for dublicates is a bit more complex than checking for rows and columns.
-Since each box contains 3x3 elements and there are 9 boxes in a board. So an outer for loop runs 3 times to for each row of boxes
-For Example:
-A sudoku board can be updated as:
-[ box1 , box2 , box3
-box4 , box5 , box6
-box7 , box8 , box9 ]
-
-For individual boxes, first, traverse vertically in the board for 3 iterations to get a column for the individual box.
-This step is repeated 3 times to get a total for 9 elements (3x3) which are then compared for dublicates using a dictionary "box".
-When the loop reaches the start of a new box i.e., column 0 , column 3, and column 6, the "box dictionary is reset to zero."
-'''
-
 def isValidSudoku(self, board: List[List[str]]) -> bool:
 row = {}
 col = {}
@@ -90,3 +58,36 @@ box = {}
                 if board[j+k][i] != ".":
                     box[board[j+k][i]] = i+j+k   
     return True
+
+
+''' SOLUTION APPROACH
+Check for problems in Individual Rows, Columns, and Boxes.
+If any row , column, or box contains a dublicate number then we return false , else true.
+
+For Dublicates in Rows:
+Iterate over each element of individual arrays (rows) in the board, and check for dublicates.
+
+For Dublicates in Columns:
+Iterate over corresponding elements of each individual array.
+For Example:
+Suppose board = [ array1, array2, .... array9]
+so, array1[0] , array2[0] ...... array9[0] form the first column of the board.
+Similarly, using a for loop for the number of elements in an array (row), we can extract individual columns
+and check for dublicates using dictionaries as above.
+
+For Dublicates in Boxes:
+
+Checking for dublicates is a bit more complex than checking for rows and columns.
+Since each box contains 3x3 elements and there are 9 boxes in a board. So an outer for loop runs 3 times to for each row of boxes
+For Example:
+A sudoku board can be updated as:
+[ box1 , box2 , box3
+box4 , box5 , box6
+box7 , box8 , box9 ]
+
+For individual boxes, first, traverse vertically in the board for 3 iterations to get a column for the individual box.
+This step is repeated 3 times to get a total for 9 elements (3x3) which are then compared for dublicates using a dictionary "box".
+When the loop reaches the start of a new box i.e., column 0 , column 3, and column 6, the "box dictionary is reset to zero."
+'''
+
+
