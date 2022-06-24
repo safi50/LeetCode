c7f3d1d9cc292806d31a14dd03ea0775e7eecb6a
safi50 <63853856+safi50@users.noreply.github.com>
2022-06-24 21:58:01 +0500

    Update ValidAnagram.py

--- ValidAnagram.py
+++ ValidAnagram.py
@@ -10,3 +10,17 @@ Example 2:
 Input: s = "rat", t = "car"
 Output: false
 '''
+
+#Hashmap Approach
+# def isAnagram(self, s: str, t: str) -> bool:     
+    # count = collections.Counter(s)    #Hashmap for s
+    # count2 = collections.Counter(t)   #Hashmap for t
+    # if count == count2:               #comparing values for hashmaps
+    #     return True
+    # return False
+    
+    
+    #Shorter Approach: 
+# def isAnagram(self, s: str, t: str) -> bool:     
+    # return True if collections.Counter(s) == collections.Counter(t) else False
+
