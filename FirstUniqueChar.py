ef2610b4f7117f2e99c63df51b35125a0e08d8a9
safi50 <63853856+safi50@users.noreply.github.com>
2022-06-24 21:36:41 +0500

    Update FirstUniqueChar.py

--- FirstUniqueChar.py
+++ FirstUniqueChar.py
@@ -1,14 +1,13 @@
 '''
 Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
 
- 
-
 Example 1:
-
 Input: s = "leetcode"
 Output: 0
-Example 2:
 
+Example 2:
 Input: s = "loveleetcode"
 Output: 2
 '''
+
+
