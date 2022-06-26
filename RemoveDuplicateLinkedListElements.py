51fe8b97177b03cab706fa40954271ea0fabd8ca
safi50 <63853856+safi50@users.noreply.github.com>
2022-06-26 19:31:02 +0500

    Update RemoveDuplicateLinkedListElements.py

--- RemoveDuplicateLinkedListElements.py
+++ RemoveDuplicateLinkedListElements.py
@@ -11,9 +11,9 @@ Output: [1,2,3]
 '''
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         temp = head
