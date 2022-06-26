12bb67206b9e7416fe1ba173505fac3aa0c04a11
safi50 <63853856+safi50@users.noreply.github.com>
2022-06-26 19:30:44 +0500

    Update RemoveDuplicateLinkedListElements.py

--- RemoveDuplicateLinkedListElements.py
+++ RemoveDuplicateLinkedListElements.py
@@ -18,7 +18,7 @@ class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         temp = head
         while temp and temp.next:
-            if temp.val == temp.next.val:
+            if temp.val == temp.next.val:   #we use if else for test cases like 1->1->1 where there is more than 1 duplicates
                 temp.next = temp.next.next
             else:
                 temp = temp.next
