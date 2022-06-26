45d748d8b56c86e8f615f4a4161f4e530cea1ef2
safi50 <63853856+safi50@users.noreply.github.com>
2022-06-26 19:33:27 +0500

    Reversing Linked Lists

--- ReverseLinkedList.py
+++ ReverseLinkedList.py
@@ -77,10 +77,10 @@ class Solution:
 		
 	#SOLUTION 3: SLOWEST -- RECURSIVE
 	# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode], prev = None) -> Optional[ListNode]:
         if not head: return prev
