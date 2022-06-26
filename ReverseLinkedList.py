d2f6e772349ea6ed823a15c2c597e063d1b777ad
safi50 <63853856+safi50@users.noreply.github.com>
2022-06-26 19:34:06 +0500

    Update ReverseLinkedList.py

--- ReverseLinkedList.py
+++ ReverseLinkedList.py
@@ -26,9 +26,9 @@ Output: []
 	
 	'''
 									#SOLUTION 1: ITERATIVE - FASTEST 
-	# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
